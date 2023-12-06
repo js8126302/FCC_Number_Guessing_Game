a21b36208266398de2bf55f1be04a0a54d617db2
js8126302 <122713123+js8126302@users.noreply.github.com>
2023-12-06 17:49:25 -0500

    feat: username prompt

--- number_guess.sh
+++ number_guess.sh
@@ -3,3 +3,7 @@ PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
 # generate random number
 RANDOM_NUMBER=$RANDOM
+# prompting user for username
+echo "Enter your username:"
+read USERNAME
+RESULT=$($PSQL SELECT * FROM )
