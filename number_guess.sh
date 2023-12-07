36b4a22a8829b8e2e2b5fc88a2ef4fd76458ceae
js8126302 <122713123+js8126302@users.noreply.github.com>
2023-12-06 19:49:35 -0500

    feat: checking result and extracting data

--- number_guess.sh
+++ number_guess.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 # generate random number
 RANDOM_NUMBER=$RANDOM
 # prompting user for username
 echo "Enter your username:"
 read USERNAME
-RESULT=$($PSQL SELECT * FROM )
+RESULT=$($PSQL "SELECT user_id, username, games_played, best_game FROM users WHERE username='$USERNAME'")
+# extracting values from result
+IFS='|' read USER_ID PSQL_USERNAME PSQL_GAMES_PLAYED PSQL_BEST_GAME <<< "$RESULT"
+
+# checking if query is empty
+if
+[[ -z "$PSQL_USERNAME" ]];
+  then
+     echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+  else
+     echo -e "Welcome back, $USERNAME! You have played $PSQL_GAMES_PLAYED games, and your best game took $PSQL_BEST_GAME guesses."
+fi
